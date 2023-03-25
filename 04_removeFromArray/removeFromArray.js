dc3bb61d45e3098e770df3c820297565eac2d4e1
OmkarKashid <68742874+OmkarKashid@users.noreply.github.com>
2023-03-25 21:37:04 +0530

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removeElements) {
+    removeElements.forEach(removeElement => {
+        array.forEach(arrayElement => {
+            if(arrayElement === removeElement){
+                let index = array.indexOf(arrayElement);
+                array.splice(index, 1);
+            }
+        });
+    });
+    return array;
 };
 
 // Do not edit below this line
