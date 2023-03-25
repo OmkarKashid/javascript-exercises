5d9dd0f34dd048b3108f06f0f7af5e4ac970cf88
OmkarKashid <68742874+OmkarKashid@users.noreply.github.com>
2023-03-25 21:00:09 +0530

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    const givenWordArr = Array.from(word);
+    let reverseWordArr=[];
+    for(let i = givenWordArr.length - 1; i>=0; i--){
+        reverseWordArr.push(givenWordArr[i]);
+    }
+    return reverseWordArr.join("");
 };
 
 // Do not edit below this line
