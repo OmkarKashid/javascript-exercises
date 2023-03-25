1929e42c4078406cfccdbdace4c7c74d1814120b
OmkarKashid <68742874+OmkarKashid@users.noreply.github.com>
2023-03-25 21:59:57 +0530

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if(!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+    if(num1<0 || num2<0) return "ERROR";
+    let min = 0;
+    let max = 0;
+    if(num2 > num1){
+        min = num1;
+        max = num2;
+    }
+    else{
+        min = num2;
+        max = num1;
+    }
+    let sum = 0;
+    for (let i = min; i<= max ; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
