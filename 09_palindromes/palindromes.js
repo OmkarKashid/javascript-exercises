59e46e346f8473a2946f72090eef9ec4406cfbc4
OmkarKashid <68742874+OmkarKashid@users.noreply.github.com>
2023-04-09 19:16:25 +0530

    Complete 09_Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const changedWord = word.toLowerCase().replace(/[^a-z]/g, "");
+  return changedWord.split("").reverse().join("") == changedWord;
 };
 
 // Do not edit below this line
