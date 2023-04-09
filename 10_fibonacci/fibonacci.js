50e704e58ce94ebc086e80c3ddf1756c0b466b4b
OmkarKashid <68742874+OmkarKashid@users.noreply.github.com>
2023-04-09 19:32:17 +0530

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    let fibArray = [1, 1, 2];
+    for (let i = 3 ; i < index; i++){
+        fibArray[i] = fibArray[i-1] + fibArray[i-2];
+    }
+    return index > 0 ? fibArray[index - 1] : "OOPS";
 };
 
 // Do not edit below this line
